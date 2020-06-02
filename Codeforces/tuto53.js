96efe966f4b658739bbd606085322aa1e1af2510
mostofa22397 <mostofa22397@gmail.com>
2020-06-03 02:45:42 +0600

    commit

---
+++ Codeforces/tuto53.js
@@ -0,0 +1,19 @@
+console.log('This is console log')
+function greet(name,greettext="This is default value"){
+    console.log(greettext+" "+ name)
+    console.log(name+" is a boy.")
+
+}
+function sum(a,b,c){
+    let d=a+b+c;
+    return d;
+} 
+let a='Ahammed';
+let b='Mostofa';
+let c='Musfiq';
+let greettext="Good Morning";
+greet(a,greettext)
+greet(b,greettext)
+greet(c,)
+let returns=sum(1,2,3)
+console.log(returns)
